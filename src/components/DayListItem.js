01807313261404da094938fe968325dcc4c64d56
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-17 00:00:46 -0800

    Add styling for spots condition if 0

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,7 +6,7 @@ import "components/DayListItem.scss";
 export default function DayListItem(props) {
   const listClasses = classNames("day-list__item", {
     "day-list__item--selected" : props.selected,
-    "day-list__item--full" : props.spots
+    "day-list__item--full" : props.spots === 0
   });
 
   const formatSpots = (props) => {
