6eab872e0bfffcbd84141b97a01c345f8c93e9d4
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-16 23:54:37 -0800

    Add function to create DayListItem components and display them in the DayList

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import DayListItem from "components/DayListItem";
+
 export default function DayList(props) {
+  const days = props.days.map(day => 
+    <DayListItem id={day.id} 
+                 name={day.name} 
+                 spots={day.spots} 
+                 selected={day.name === props.day}
+                 setDay={props.setDay} 
+    />
+  );
+
   return (
-    <ul></ul>
+    <ul>{days}</ul>
   );
 }
