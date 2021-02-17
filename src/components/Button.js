048ef2dbc1adb31a8585082eae26ef422aae8939
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-16 23:21:26 -0800

    Change let to const

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClasses = classNames("button", {
+   const buttonClasses = classNames("button", {
       "button--confirm" : props.confirm,
       "button--danger" : props.danger
    });
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
-  let listClasses = classNames("day-list__item", {
+  const listClasses = classNames("day-list__item", {
     "day-list__item--selected" : props.selected,
     "day-list__item--full" : props.spots
   });
