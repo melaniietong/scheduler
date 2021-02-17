91061aa2981abfe93671f292b51b29ee2a8748c4
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-17 00:39:23 -0800

    Add dynamic class names

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import classNames from "classnames";
+
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const interviewerClasses = classNames("interviewers__item", {
+    "interviewers__item--selected" : props.selected
+  });
+
+  const interviewerImageClasses = classNames("interviewers__item-image", {
+    "interviewers__item--selected-image" : props.selected
+  });
+
+  return (
+    <li className={interviewerClasses}>
+      <img className={interviewerImageClasses}
+           src={props.avatar}
+           alt={props.name}
+      />
+      {props.name}
+    </li>
+  );
+}
