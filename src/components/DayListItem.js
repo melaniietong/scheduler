ca3e2e2516da5690aff6920d0e895816bd115ba0
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-16 23:38:46 -0800

    Add formatting for spots remaining display

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,20 @@ export default function DayListItem(props) {
     "day-list__item--full" : props.spots
   });
 
+  const formatSpots = (props) => {
+    if (props.spots > 1) {
+      return props.spots + " spots remaining"
+    } else if (props.spots === 1) {
+      return props.spots + " spot remaining"
+    } else {
+      return "no spots remaining"
+    }
+  };
+
   return (
     <li className={listClasses} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props)}</h3>
     </li>
   );
 }
