c99ee0cd76053419753dece2b7e3c57e5eb87fc7
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-17 00:43:23 -0800

    Add initial data and style to InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import InterviewerListItem from "components/InterviewerListItem";
+
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list"></ul>
+    </section>
+  );
+}
