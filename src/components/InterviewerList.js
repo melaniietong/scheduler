68d36c9596b1d46b2fb539b3ca675c69637b3f7f
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-17 01:03:30 -0800

    Add dynamic content

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,10 +5,18 @@ import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map(interviewer => 
+    <InterviewerListItem id={interviewer.id} 
+                         name={interviewer.name}
+                         avatar={interviewer.avatar} 
+                         selected={props.interviewer === interviewer.id}
+    />
+  );
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviewers}</ul>
     </section>
   );
 }
