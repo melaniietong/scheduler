556692eceecca6099ac472fd181b43ca22eccc12
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-17 19:01:24 -0800

    Add name hook

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,58 +6,33 @@ import InterviewerList from "components/InterviewerList.js";
 export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer ? props.interviewer.id : null);
 
-  if (props.name) {
-    return (
-      <main className="appointment__card appointment__card--create">
-        <section className="appointment__card-left">
-          <form autoComplete="off">
-            <input className="appointment__create-input text--semi-bold"
-                   name="name"
-                   type="text"
-                   value={props.name}
-                   placeholder="Enter Student Name"
-            />
-          </form>
-          
-          <InterviewerList interviewers={props.interviewers}
-                           interviewer={interviewer}
-                           setInterviewer={setInterviewer}
+  const [name, setName] = useState(props.name ? props.name : "");
+
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input className="appointment__create-input text--semi-bold"
+                 name="name"
+                 type="text"
+                 value={name}
+                 onChange={(event) => setName(event.target.value)}
+                 placeholder="Enter Student Name"
           />
-        </section>
+        </form>
         
-        <section className="appointment__card-right">
-          <section className="appointment__actions">
-            <Button onClick={props.onCancel} danger>Cancel</Button>
-            <Button onClick={props.onSave} confirm>Save</Button>
-          </section>
+        <InterviewerList interviewers={props.interviewers}
+                         interviewer={interviewer}
+                         setInterviewer={setInterviewer}
+        />
+      </section>
+      
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
         </section>
-      </main>
-    );
-  } else {
-    return (
-      <main className="appointment__card appointment__card--create">
-        <section className="appointment__card-left">
-          <form autoComplete="off">
-            <input className="appointment__create-input text--semi-bold"
-                   name="name"
-                   type="text"
-                   placeholder="Enter Student Name"
-            />
-          </form>
-          
-          <InterviewerList interviewers={props.interviewers}
-                           interviewer={interviewer}
-                           setInterviewer={setInterviewer}
-          />
-        </section>
-        
-        <section className="appointment__card-right">
-          <section className="appointment__actions">
-            <Button onClick={props.onCancel} danger>Cancel</Button>
-            <Button onClick={props.onSave} confirm>Save</Button>
-          </section>
-        </section>
-      </main>
-    );
-  }
+      </section>
+    </main>
+  );
 }
