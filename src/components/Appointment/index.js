f20ce5a85c13fc255182c0e69a5517e3f26ced56
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-19 03:21:10 -0800

    Add import for useVisualMode custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,6 +4,8 @@ import Empty from "components/Appointment/Empty.js";
 import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show.js";
 
+import { useVisualMode } from "hooks/useVisualMode";
+
 import "components/Appointment/styles.scss";
 
 export default function Appointment(props) {
