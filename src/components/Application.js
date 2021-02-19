c11a33952efd04a93d61a9b1bc64e5c13011cc15
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-18 23:50:20 -0800

    Add hardcoded appointments

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,29 +1,77 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 import "components/Application.scss";
 
-export default function Application(props) {
-  const days = [
-    {
-      id: 1,
-      name: "Monday",
-      spots: 2,
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      spots: 5,
-    },
-    {
-      id: 3,
-      name: "Wednesday",
-      spots: 0,
-    },
-  ];
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "3pm",
+    interview: {
+      student: "Grover McKinney",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png"
+      }
+    }
+  },
+  {
+    id: 4,
+    time: "3:30pm",
+    interview: {
+      student: "Loren Mansell",
+      interviewer: {
+        id: 5,
+        name: "Sven Jones",
+        avatar: "https://i.imgur.com/twYrpay.jpg"
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "2:15pm",
+    interview: {
+      student: "Rebecca Pineda",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg"
+      }
+    }
+  }
+];
 
+export default function Application(props) {
   const [currentday, setCurrentDay] = useState("Monday");
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    const testURL = 'http://localhost:8001/api/days';
+    axios.get(testURL).then(response => {
+      console.log(response.data);
+      setDays([...response.data])
+    });
+  }, [])
   
   return (
     <main className="layout">
@@ -48,7 +96,13 @@ export default function Application(props) {
       </section>
       
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => {
+          return (
+            <Appointment key={appointment.id} {...appointment} />
+
+          );
+        })}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
