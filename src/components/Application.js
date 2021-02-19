9e6a228d57c18ad0b93c196cac1801ec433135a5
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-19 03:17:41 -0800

    Add import for useVisualMode custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,7 @@ import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 
 import { getAppointmentsForDay, getInterview } from 'helpers/selectors';
+import { useVisualMode } from "hooks/useVisualMode"
 
 import "components/Application.scss";
 
