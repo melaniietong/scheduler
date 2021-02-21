c98178c4043b1efe1e4055e22f6753843209b371
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-20 16:03:52 -0800

    Add to bookInterview() function to save the new appointment

--- src/components/Application.js
+++ src/components/Application.js
@@ -36,7 +36,20 @@ export default function Application(props) {
   const dailyInterviewers = getInterviewersForDay(state, state.day);
 
   const bookInterview = (id, interview) => {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+
+    setState({
+      ...state,
+      appointments
+    });
   };
   
   return (
