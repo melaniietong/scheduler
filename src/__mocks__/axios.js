161952736bc778503c6185ef36cb533c94439adb
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-24 15:56:44 -0800

    Add export

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -1,3 +1,31 @@
+export default {
+  get: jest.fn(url => {
+    if (url === "/api/days") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "/api/appointments") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+
+    if (url === "/api/interviewers") {
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  })
+}
+
 const fixtures = {
   days: [
     {
