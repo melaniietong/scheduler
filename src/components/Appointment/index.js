ee7d87d97154e798ce4693253f62b74a1ed1fb00
Melanie Tong <melanie@Melanies-MacBook-Air.local>
2021-02-24 18:22:32 -0800

    Add data-testid to <article> tag

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -47,7 +47,7 @@ export default function Appointment(props) {
   }
 
   return (
-    <article className="appointment">
+    <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
       {mode === CONFIRM && <Confirm
         message="Are you sure you want to delete this appointment?"
